932e098acedcbb28c89dd0cf1e47f754ae7b47b7
Shlok Mehrotra <shlokmehrotra@gmail.com>
2022-10-07 09:11:53 -0500

    search captions folder

--- vtt_parse.py
+++ vtt_parse.py
@@ -1,13 +1,32 @@
 import webvtt
 from collections import defaultdict
 
+file_name = "1_1x0b27pk-en_US.vtt"
+
+
+#Class Transcribe caption files
 vtt = webvtt.read("1_1x0b27pk-en_US.vtt")
 dictionary = defaultdict(int)
 for line in vtt:
-    # print(line.start)
-    # print(line.end)
-    # print(line.text)
     for words in line.text.split():
         dictionary[words] += 1
 
 print(list(sorted(dictionary.items(), key = lambda x: x[1], reverse = True))[:5])
+
+#Now comparing with DRES
+# os.system("cd ExpertCaptioner-Captions")
+
+os.chdir("ExpertCaptioner-Captions")
+
+search = "ls " + str(file_name.split('-')[0]) + "-*meta.json"
+print(search)
+
+cands = os.popen(search).readlines()
+
+for cand in cands:
+    cand = cand.strip()
+    with open(cand) as json_file:
+        content = json.load(json_file)
+        if content["displayOnPlayer"]:
+            end = content["mediaId"]
+            break
