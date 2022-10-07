42162ed10f3fb0d94b4158f502b86703aa351b9c
Jessica Singh <jessicasingh@outlook.com>
2022-10-06 23:26:24 -0500

    reads all test files

--- readingFiles.py
+++ readingFiles.py
@@ -1,4 +1,18 @@
 import json
-with open("captionsParsing/testFiles/0_247k9qz7-0_34d7z15g-meta.json") as json_file:
-    metadata = json.load(json_file)
-    print(metadata)
+import os
+ 
+# Get the list of all files and directories
+path = "testFiles/"
+files = os.listdir(path)
+
+print(files)
+
+captionsBeingDisplayed = ""
+
+for file in files:
+    with open(path+file) as json_file:
+        metadata = json.load(json_file)
+        if metadata["displayOnPlayer"] == True:
+            captionsBeingDisplayed = file
+        
+print(captionsBeingDisplayed)
